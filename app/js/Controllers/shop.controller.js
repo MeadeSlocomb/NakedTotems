2bc12cdae7cb76e8a6898ad432875f32978e54f9
Meade Slocomb <meade.slocomb@gmail.com>
2015-09-10 15:31:32 -0400

    Added sample home page pictures and hover transitions, fleshed out router and paths

---
+++ app/js/Controllers/shop.controller.js
@@ -0,0 +1,15 @@
+(function (){
+
+  'use strict';
+
+  angular.module('NakedTotems')
+
+  .controller('ShopController', ['$scope',
+
+    function ($scope) {
+
+    }
+
+  ]);
+
+}());
--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,46 @@
           url: '/',
           templateUrl: 'templates/home.tpl.html',
           controller: 'MainController'
+      })
+        .state('shapes', {
+          url: '/shapes/',
+          templateUrl: 'templates/shapes.tpl.html',
+          controller: 'ShopController'
+      })
+        .state('birdbaths', {
+          url: '/birdbaths/',
+          templateUrl: 'templates/birdbaths.tpl.html',
+          controller: 'ShopController'
+      })
+        .state('totems', {
+          url: '/totems/',
+          templateUrl: 'templates/totems.tpl.html',
+          controller: 'ShopController'
+      })
+      .state('tables', {
+          url: '/totems/',
+          templateUrl: 'templates/totems.tpl.html',
+          controller: 'ShopController'
+      })
+      .state('mirrors', {
+          url: '/mirrors/',
+          templateUrl: 'templates/totems.tpl.html',
+          controller: 'ShopController'
+      })
+        .state('materials', {
+          url: '/materials/',
+          templateUrl: 'templates/materials.tpl.html',
+          controller: 'ShopController'
+      })
+        .state('about', {
+          url: '/about/',
+          templateUrl: 'templates/about.tpl.html',
+          controller: 'MainController'
+      })
+        .state('contact', {
+          url: '/contact/',
+          templateUrl: 'templates/contact.tpl.html',
+          controller: 'MainController'
       });
 
     }
